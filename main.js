da1fada90d4b5d8b8fb6fff0144014cff944e290
Sebastien Derilus <sebastienderilus@Sebastiens-MacBook-Pro.local>
2023-04-02 11:27:03 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let x = 1;
+let y = 2;
+let z = 3;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+export { x, y, z, add, subtract, multiply };
---
+++ utils.js
@@ -0,0 +1,9 @@
+import { x, y, z, add, subtract, multiply } from './main.js';
+
+
+console.log(x); // x should =1
+console.log(y); // y should =2
+console.log(z); // z should =3
+console.log(add(x, y)); // 1+2=3
+console.log(subtract(z, x)); //3-1= 2
+console.log(multiply(y, z)); // 2*3=6
